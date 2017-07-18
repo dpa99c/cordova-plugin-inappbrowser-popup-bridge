85456766cf82f9d0df0756bfb33bda5c6e34a010
Dave Alden <dpa99c@gmail.com>
2017-07-18 12:11:13 +0100

    Fix: Override PopupBridge to force min SDK from API 19 to API 16.

---
+++ src/android/override_PopupBridge_minSdkVersion.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    path = require('path');
+
+module.exports = function (context) {
+    var deferral = context.requireCordovaModule('q').defer();
+
+
+    var overrideAttribute = "tools:overrideLibrary=\"com.braintreepayments.popupbridge\" ";
+    var usesSdkOpen = "<uses-sdk ";
+
+    var projectRoot = context.opts.projectRoot;
+    var platformRoot = path.join(projectRoot, 'platforms/android');
+    var manifestPath = path.join(platformRoot, 'AndroidManifest.xml');
+    //console.log("manifestPath: " + manifestPath);
+
+    fs.readFile(manifestPath, function(err, manifest) {
+        if(err || !manifest){
+            deferral.reject("Failed to read AndroidManifest.xml: " + err);
+        }
+
+        manifest = manifest.toString();
+
+        if(manifest.indexOf(overrideAttribute) == -1){
+            //console.log("not found");
+
+            manifest = manifest.replace(usesSdkOpen, usesSdkOpen + overrideAttribute);
+
+            fs.writeFile(manifestPath, manifest, 'utf8', function (err) {
+                if (err) {
+                    deferral.reject("Failed to write AndroidManifest.xml: " + err);
+                }
+                //console.log("wrote");
+                deferral.resolve();
+            });
+        }else{
+            //console.log("found");
+        }
+
+    });
+    return deferral.promise;
+};
